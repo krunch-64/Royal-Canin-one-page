55fa13efc128d4c0bbf022355d614d696983430b
Romain Bailhache <romain.bailhache@gmail.com>
2022-10-19 14:18:15 +0200

    call api

--- assets/js/api-cat.js
+++ assets/js/api-cat.js
@@ -1,7 +1,43 @@
-const api_key = '?api_key=live_XksD8awTUhN7EJSio8f37G1NL15oDGdKHWrOywdZRf3S3QejhcUGcJmscBRl3wOU';
-const request = require('request');
-request('https://api.thecatapi.com/v1/breeds'+api_key, function (error, response, data) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log(data); // Print the data.
+const url = `https://api.thecatapi.com/v1/breeds`;
+const api_key = "live_XksD8awTUhN7EJSio8f37G1NL15oDGdKHWrOywdZRf3S3QejhcUGcJmscBRl3wOU"
+
+var tags = [];
+
+let storedBreeds = []
+
+ fetch(url,{headers: {
+      'x-api-key': api_key
+    }})
+ .then((response) => {
+   return response.json();
+ })
+.then((data) => {
+   
+   //filter to only include those with an `image` object
+   data = data.filter(img=> img.image?.url!=null)
+   
+  storedBreeds = data;
+   
+   for (let i = 0; i < storedBreeds.length; i++) {
+    const breed = storedBreeds[i];
+    let option = document.createElement('option');
+    
+     
+     //skip any breeds that don't have an image
+     if(!breed.image)continue
+     
+    //use the current array index
+    option.value = i;
+    option.innerHTML = `${breed.name}`;
+document.getElementById('breed_selector').appendChild(option);
+    
+    }
+})
+.catch(function(error) {
+   console.log(error);
 });
+
+function showBreedImage(index)
+{ 
+  document.getElementById("breed_image").src= storedBreeds[index].image.url;
+}
---
+++ assets/js/caculate_years_cats.js
@@ -0,0 +1,17 @@
+let test = document.getElementById('input_years');
+let years_or_month = document.getElementById('input_select_years')
+
+const form = document.getElementById('form-years')
+form.addEventListener('submit',check = (e) => {
+    e.preventDefault();
+    console.log("form")
+})
+//document.getElementById('btn_years').addEventListener('click', calculated_years);
+
+function calculated_years() {
+
+}
+
+function convert_years_on_month(years) {
+
+}
